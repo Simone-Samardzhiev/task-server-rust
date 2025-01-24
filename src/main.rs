6c0fddd5b6636744dd26ae4b6b8818bee4e180c3
Simone <simonesamardzhiev@gmail.com>
2025-01-24 12:41:34 +0200

    Add config module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+use sqlx::{Pool, Postgres};
+
+pub mod config;
+
+pub struct AppState {
+    pub pool: Pool<Postgres>,
+    pub jwt_secret: String,
+}
+
+impl AppState {
+    pub fn new(pool: Pool<Postgres>, jwt_secret: String) -> Self {
+        Self { pool, jwt_secret }
+    }
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let config = config::load::Config::load();
+    let pool = sqlx::postgres::PgPool::connect(&config.database_url)
+        .await
+        .expect("Failed to connect to database");
+
+    let app_state = AppState::new(pool, config.jwt_secret);
+
+    HttpServer::new(move || App::new().app_data(web::Data::new(app_state)))
+        .bind(config.server_socket)?
+        .run()
+        .await?;
+
+    Ok(())
 }
