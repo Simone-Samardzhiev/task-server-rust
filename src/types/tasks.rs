db306dbc2c00cd0118f2d185d02b9ba049eb1991
Simone <simonesamardzhiev@gmail.com>
2025-01-28 20:16:21 +0200

    Add new methods in task types

--- src/types/tasks.rs
+++ src/types/tasks.rs
@@ -13,6 +13,40 @@ pub struct Task {
     pub date_deleted: Option<DateTime<Utc>>,
 }
 
+impl Task {
+    pub fn new(
+        id: uuid::Uuid,
+        name: String,
+        description: String,
+        r#type: i32,
+        due_date: DateTime<Utc>,
+        date_completed: Option<DateTime<Utc>>,
+        date_deleted: Option<DateTime<Utc>>,
+    ) -> Self {
+        Self {
+            id,
+            name,
+            description,
+            r#type,
+            due_date,
+            date_completed,
+            date_deleted,
+        }
+    }
+
+    pub fn new_from_task_payload(payload: &TaskPayload) -> Self {
+        Self::new(
+            uuid::Uuid::new_v4(),
+            payload.name.clone(),
+            payload.description.clone(),
+            payload.r#type,
+            payload.due_date,
+            None,
+            None,
+        )
+    }
+}
+
 #[derive(Debug, Deserialize)]
 pub struct TaskPayload {
     pub name: String,
