6cc7ff28dcf59680317f8fc347bdf7977f214134
Simone <simonesamardzhiev@gmail.com>
2025-03-18 12:47:11 +0200

    Start refactoring the project

--- src/auth/passwords.rs
+++
@@ -1,9 +0,0 @@
-use bcrypt::{hash, verify, BcryptResult, DEFAULT_COST};
-
-pub fn hash_password(password: &str) -> BcryptResult<String> {
-    hash(password, DEFAULT_COST)
-}
-
-pub fn verify_password(password: &str, hash: &str) -> bool {
-    verify(password, hash).unwrap_or(false)
-}
