7ac746177a0e7f243c52ba4204325fee573228c3
Simone <simonesamardzhiev@gmail.com>
2025-04-07 11:29:11 +0300

    Refactor project structure and config.rs file for loading configuration

---
+++ src/lib/config.rs
@@ -0,0 +1,28 @@
+//! `config` module used to load configuration.
+use std::env::{var, VarError};
+
+/// `SERVER_ADDR_KEY` used to access server address.
+const SERVER_ADDR_KEY: &str = "SERVER_ADDR";
+
+/// `DATABASE_URL_KEY` used to access database url.
+const DATABASE_URL_KEY: &str = "DATABASE_URL";
+
+/// `Config` holds configuration.
+pub struct Config {
+    /// `server_addr` holds the serer address.
+    pub server_addr: String,
+    /// `database_url` holds the database url.
+    pub database_url: String,
+}
+
+impl Config {
+    /// `new_from_env` will load the configuration from the environment variables.
+    pub fn new_from_env() -> Result<Self, VarError> {
+        let server_addr = var(SERVER_ADDR_KEY)?;
+        let database_url = var(DATABASE_URL_KEY)?;
+        Ok(Self {
+            server_addr,
+            database_url,
+        })
+    }
+}
