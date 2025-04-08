86c0713596ec2da442893be43747aa00311fe858
Simone <simonesamardzhiev@gmail.com>
2025-04-08 22:46:21 +0300

    Add secret for JWT in config.rs

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -7,12 +7,17 @@ const SERVER_ADDR_KEY: &str = "SERVER_ADDR";
 /// `DATABASE_URL_KEY` used to access database url.
 const DATABASE_URL_KEY: &str = "DATABASE_URL";
 
+/// `SECRET_KEY` used to access JWT secret.
+const SECRET_KEY: &str = "SECRET";
+
 /// `Config` holds configuration.
 pub struct Config {
     /// `server_addr` holds the serer address.
     pub server_addr: String,
     /// `database_url` holds the database url.
     pub database_url: String,
+    /// `secret` hold JWT secret
+    pub secret: String,
 }
 
 impl Config {
@@ -20,9 +25,11 @@ impl Config {
     pub fn new_from_env() -> Result<Self, VarError> {
         let server_addr = var(SERVER_ADDR_KEY)?;
         let database_url = var(DATABASE_URL_KEY)?;
+        let secret = var("SECRET")?;
         Ok(Self {
             server_addr,
             database_url,
+            secret,
         })
     }
 }
